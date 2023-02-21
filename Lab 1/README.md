ef1cacfbbc4277c846b4b7cebdb3d5124c6e8675
EmmaGirigan <75792071+EmmaGirigan@users.noreply.github.com>
2023-02-21 11:19:05 +0200

    compulsory lab 1

--- Lab 1/README.md
+++
@@ -1,2 +0,0 @@
-# ProgramareAvansata
-Teme programare avansata
--- Lab1/src/main/java/Compulsory.java
+++ Lab1/src/main/java/Compulsory.java
@@ -1,24 +1,25 @@
 public class Compulsory {
-    private static int sum(int result){
-        int sum = 0;
-        while(result != 0){
-            sum += result % 10;
-            result /= 10;
-        }
-        return sum;
-    }
-    public static void main(String[] args){
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
         String[] languages = {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
-        int n = (int) (Math.random() * 1000000);
+        int n = (int) (Math.random() * 1_000_000);
         n *= 3;
         n += Integer.parseInt("10101", 2);
-        n += Integer.parseInt("FF", 16);
+        n += Integer.parseInt("FF",16);
         n *= 6;
         int result = sum(n);
         while(result > 9){
             result = sum(result);
         }
         System.out.println("Willy-nilly, this semester I will learn " + languages[result]);
+    }
+    private static int sum(int number) {
+        int sum = 0;
+        while (number != 0)
+        {
+            sum = sum + number % 10;
+            number = number/10;
+        }
+        return sum;
     }
 }
